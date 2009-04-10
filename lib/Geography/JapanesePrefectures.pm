f99f72c62a7cea559f6fa1e71f3c17872ea4a0b3
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2009-04-10 07:32:14 +0000

    flagged utf8 is better way =)
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Geography-JapanesePrefectures/trunk@32229 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Geography/JapanesePrefectures.pm
+++ lib/Geography/JapanesePrefectures.pm
@@ -3,89 +3,51 @@ use strict;
 use warnings;
 use 5.00800;
 our $VERSION = '0.06';
-use Carp;
 
+use Geography::JapanesePrefectures::Unicode;
+use Carp   ();
+use Encode ();
 use List::MoreUtils qw(uniq);
-
-our $PREFECTURES = [
-    { id => 1,  name => '北海道',    region => '北海道' },
-    { id => 2,  name => '青森県',    region => '東北' },
-    { id => 3,  name => '岩手県',    region => '東北' },
-    { id => 4,  name => '宮城県',    region => '東北' },
-    { id => 5,  name => '秋田県',    region => '東北' },
-    { id => 6,  name => '山形県',    region => '東北' },
-    { id => 7,  name => '福島県',    region => '東北' },
-    { id => 8,  name => '茨城県',    region => '関東' },
-    { id => 9,  name => '栃木県',    region => '関東' },
-    { id => 10, name => '群馬県',    region => '関東' },
-    { id => 11, name => '埼玉県',    region => '関東' },
-    { id => 12, name => '千葉県',    region => '関東' },
-    { id => 13, name => '東京都',    region => '関東' },
-    { id => 14, name => '神奈川県', region => '関東' },
-    { id => 15, name => '新潟県',    region => '信越' },
-    { id => 16, name => '富山県',    region => '北陸' },
-    { id => 17, name => '石川県',    region => '北陸' },
-    { id => 18, name => '福井県',    region => '北陸' },
-    { id => 19, name => '山梨県',    region => '関東' },
-    { id => 20, name => '長野県',    region => '信越' },
-    { id => 21, name => '岐阜県',    region => '東海' },
-    { id => 22, name => '静岡県',    region => '東海' },
-    { id => 23, name => '愛知県',    region => '東海' },
-    { id => 24, name => '三重県',    region => '東海' },
-    { id => 25, name => '滋賀県',    region => '近畿' },
-    { id => 26, name => '京都府',    region => '近畿' },
-    { id => 27, name => '大阪府',    region => '近畿' },
-    { id => 28, name => '兵庫県',    region => '近畿' },
-    { id => 29, name => '奈良県',    region => '近畿' },
-    { id => 30, name => '和歌山県', region => '近畿' },
-    { id => 31, name => '鳥取県',    region => '中国' },
-    { id => 32, name => '島根県',    region => '中国' },
-    { id => 33, name => '岡山県',    region => '中国' },
-    { id => 34, name => '広島県',    region => '中国' },
-    { id => 35, name => '山口県',    region => '中国' },
-    { id => 36, name => '徳島県',    region => '四国' },
-    { id => 37, name => '香川県',    region => '四国' },
-    { id => 38, name => '愛媛県',    region => '四国' },
-    { id => 39, name => '高知県',    region => '四国' },
-    { id => 40, name => '福岡県',    region => '九州' },
-    { id => 41, name => '佐賀県',    region => '九州' },
-    { id => 42, name => '長崎県',    region => '九州' },
-    { id => 43, name => '熊本県',    region => '九州' },
-    { id => 44, name => '大分県',    region => '九州' },
-    { id => 45, name => '宮崎県',    region => '九州' },
-    { id => 46, name => '鹿児島県', region => '九州' },
-    { id => 47, name => '沖縄県',    region => '沖縄' },
-];
 
 sub prefectures {
     my $self = shift;
-    return map { $_->{name} } @$PREFECTURES;
+    return
+      map { Encode::encode_utf8($_) }
+      Geography::JapanesePrefectures::Unicode->prefectures();
 }
 
 sub regions {
     my $self = shift;
-    return uniq map { $_->{region} } @$PREFECTURES;
+    return
+      map { Encode::encode_utf8($_) }
+      Geography::JapanesePrefectures::Unicode->regions();
 }
 
 sub prefectures_in {
     my ( $self, $region ) = @_;
-    return map { $_->{name} } grep { $_->{region} eq $region } @$PREFECTURES;
+    return
+      map { Encode::encode_utf8($_) }
+      Geography::JapanesePrefectures::Unicode->prefectures_in(
+        Encode::decode_utf8($region) );
 }
 
 sub prefectures_id {
     my ( $self, $prefecture ) = @_;
-
-    for my $pref (@$PREFECTURES) {
-        if ( $prefecture eq $pref->{name} ) {
-            return $pref->{id};
-        }
-    }
+    return Geography::JapanesePrefectures::Unicode->prefectures_id(
+        Encode::decode_utf8($prefecture) );
 }
 
 sub prefectures_infos {
-    my ($self, $args) = @_;
-
-    return $PREFECTURES;
+    my $infos = Geography::JapanesePrefectures::Unicode->prefectures_infos;
+    my @ret;
+    for my $info (@$infos) {
+        my %row;
+        while (my ($key, $val) = each %$info) {
+            $row{$key} = Encode::encode_utf8($val);
+        }
+        push @ret, \%row;
+    }
+    return \@ret;
 }
 
 1;
@@ -99,62 +61,10 @@ __END__
 
 Geography::JapanesePrefectures - Japanese Prefectures Data.
 
-=head1 SYNOPSIS
-
-    use Geography::JapanesePrefectures;
-
-    Geography::JapanesePrefectures->prefectures_in('関東');
-    # => qw(茨城県 栃木県 群馬県 埼玉県 千葉県 東京都 神奈川県 山梨県)
-    
-    Geography::JapanesePrefectures->prefectures_id('東京');
-    # => 13
-
 =head1 DESCRIPTION
 
-This module allows you to get information on Japanese Prefectures names. and region.
-
-=head1 Class Methods
-
-=head2 prefectures
-
-    my @prefectures = Geography::JapanesePrefectures->prefectures;
-
-get the prefectures names.
-
-=head2 regions
-
-    my @regions = Geography::JapanesePrefectures->regions;
-
-get the region names.
-
-=head2 prefectures_in
-
-    my @prefectures = Geography::JapanesePrefectures->prefectures_in('関東');
-    # => qw(茨城県 栃木県 群馬県 埼玉県 千葉県 東京都 神奈川県 山梨県)
-
-get prefectures in region.
-
-=head2 prefectures_id
-
-    Geography::JapanesePrefectures->prefectures_id('和歌山県');
-    # => 30
-
-get prefecture's ID.
-
-=head2 prefectures_infos
-
-    Geography::JapanesePrefectures->prefectures_infos();
-    # => [ { id => 1,  name => '北海道',    region => '北海道' }, ... ]
-
-get all informations.
-
-=head1 THANKS TO
-
-    Tatsuhiko Miyagawa
-    Yappo(Acme::Oppai)
-    nipotan
-    Shot(for greeting)
-    nekokak
+This module is deprecated.do not use directly.
+You should call Geography::JapanesePrefectures::Unicode instead of this.
 
 =head1 AUTHOR
 
