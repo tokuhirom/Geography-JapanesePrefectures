a3d2b865244fba80339fe43f2fd0688a9413ad5e
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-04-10 21:24:00 +0900

    Checking in changes prior to tagging of version 0.07.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 4990fc5..e3ba29f 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,4 +1,10 @@
     Revision history for Perl extension Geography::JapanesePrefectures
    
    +0.07
    +
    +        - added Geography::JapanesePrefectures::Unicode for
    +          valid unicode handling
    +        - roman support by lopnor++
    +
     0.01    Mon Feb  4 09:45:57 2008
             - original version

--- lib/Geography/JapanesePrefectures.pm
+++ lib/Geography/JapanesePrefectures.pm
@@ -2,7 +2,7 @@ package Geography::JapanesePrefectures;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 use Geography::JapanesePrefectures::Unicode;
 use Carp   ();
