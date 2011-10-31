f42cb9f58a47233bd7d594e7b02d85ea4fc00238
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-10-31 10:14:56 +0900

    Checking in changes prior to tagging of version 0.10.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 515c4f1..f883373 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Geography::JapanesePrefectures
    
    +0.10
    +
    +        - fixed testing issue
    +        - modernize packaging
    +
     0.09
    
             - fixed typo(nekokak)

--- lib/Geography/JapanesePrefectures.pm
+++ lib/Geography/JapanesePrefectures.pm
@@ -2,7 +2,7 @@ package Geography::JapanesePrefectures;
 use strict;
 use warnings;
 use 5.008001;
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 use Geography::JapanesePrefectures::Unicode;
 use Carp   ();
